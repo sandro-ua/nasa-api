7c73852d0d9f0fe329283d59d18cab8f7a80012b
sandro-ua <alexandr.post.box@gmail.com>
2024-01-23 19:42:22 +0200

    Get back to original DSL

--- cicd/JenkinsDSL.groovy
+++ cicd/JenkinsDSL.groovy
@@ -2,8 +2,8 @@ pipelineJob('NASA_REGRESSION_DSL_GENERATED') {
     definition {
         cpsScm {
             scm {
-                github('https://github.com/sandro-ua/nasa-api')
-            }       
+                git('https://github.com/sandro-ua/nasa-api')
+            }
 			scriptPath('JenkinsFile.groovy')
         }
     }
