f70460f6375872f3389e0252839d0977ce240a9d
sandro-ua <alexandr.post.box@gmail.com>
2024-01-23 19:10:07 +0200

    Fix scm issue in dsl. 2nd attempt

--- cicd/JenkinsDSL.groovy
+++ cicd/JenkinsDSL.groovy
@@ -2,15 +2,9 @@ pipelineJob('NASA_REGRESSION_DSL_GENERATED') {
     definition {
         cpsScm {
             scm {
-                git {
-                    // Git repository URL
-                    remote {
-                        git 'https://github.com/sandro-ua/nasa-api'
-                    }
-                    branch 'master'
-                }
+                git('https://github.com/sandro-ua/nasa-api')
             }
-            scriptPath('JenkinsFile.groovy')
+			scriptPath('JenkinsFile.groovy')
         }
     }
 }
