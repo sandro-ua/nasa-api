47e430697d00c415c43c88a8a7082c87c6f9e0f9
sandro-ua <alexandr.post.box@gmail.com>
2024-01-23 18:41:12 +0200

    Potential fix for scm error

--- cicd/JenkinsDSL.groovy
+++ cicd/JenkinsDSL.groovy
@@ -1,14 +1,12 @@
 pipelineJob('NASA_REGRESSION_DSL_GENERATED') {
     definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        git 'https://github.com/sandro-ua/nasa-api'
-                    }
+        scm {
+            git {
+                remote {
+                    git 'https://github.com/sandro-ua/nasa-api'
                 }
             }
-            scriptPath('JenkinsFile.groovy')
         }
+        scriptPath('JenkinsFile.groovy')
     }
 }
