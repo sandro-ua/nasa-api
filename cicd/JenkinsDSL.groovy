b3d2fcb6c00d4c0b500ef8031eebeede4bbedb5c
sandro-ua <alexandr.post.box@gmail.com>
2024-01-23 19:19:00 +0200

    Change scm from git to git-hub

--- cicd/JenkinsDSL.groovy
+++ cicd/JenkinsDSL.groovy
@@ -2,8 +2,8 @@ pipelineJob('NASA_REGRESSION_DSL_GENERATED') {
     definition {
         cpsScm {
             scm {
-                git('https://github.com/sandro-ua/nasa-api')
-            }
+                github('https://github.com/sandro-ua/nasa-api')
+            }       
 			scriptPath('JenkinsFile.groovy')
         }
     }
