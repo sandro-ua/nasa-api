650594c0d30cef30412f21e43122b6e366bdceda
sandro-ua <alexandr.post.box@gmail.com>
2024-03-03 22:35:36 +0200

    Add missing file

---
+++ HTTPConnection.py
@@ -0,0 +1,30 @@
+import urllib
+
+import requests
+import constants as const
+
+
+class HTTPConnection:
+    def __init__(self):
+        self.base_url = const.BASE_URL
+
+    def send_request(self, **kwargs):
+        """Send GET request to https://api.nasa.gov/EPIC endpoint
+        Example url: https://api.nasa.gov/EPIC/api/natural/date/2019-05-30?api_key=DEMO_KEY
+        EPIC = endpoint
+        natural = param
+        date = sub_param
+        2019-05-30 = value
+        """
+
+        url = (
+            f"{self.base_url}/EPIC/api/{kwargs.get('param')}/{kwargs.get('sub_param')}/{kwargs.get('value')}?api_key={const.API_KEY}")
+        response = requests.request("GET", url)
+        return response
+
+    def parse_response(self, response):
+        try:
+            data = response.json()
+            return data
+        except ValueError:
+            return "Failed to parse response"
