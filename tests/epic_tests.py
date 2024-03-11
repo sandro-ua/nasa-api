3fc2d691790f3054a45f339988f86d020ae189e4
sandro-ua <qatestermailbox@gmail.com>
2024-03-11 21:54:39 +0200

    Build url by params

--- tests/epic_tests.py
+++ tests/epic_tests.py
@@ -6,13 +6,13 @@ import logger as log
 def test_nasa_image_response_code():
     # url = f"{const.BASE_URL}/EPIC/api/natural/date/2019-05-30?api_key={const.API_KEY}"
     http_c = http.HTTPConnection()
-    response = http_c.send_request(param="natural", sub_param="date", value="2019-05-30")
+    response = http_c.send_request(method="GET", param="natural", sub_param="date", value="2019-05-30")
     assert response.status_code == 200, f"Request failed with status code {response.status_code}"
 
 
 def test_nasa_image_key():
     http_c = http.HTTPConnection()
-    response = http_c.send_request(param="natural", sub_param="date", value="2019-05-30")
+    response = http_c.send_request(method="GET", param="natural", sub_param="date", value="2019-05-30")
 
     assert response.status_code == 200, f"Request failed with status code {response.status_code}"
     data = response.json()
@@ -29,15 +29,10 @@ def test_nasa_image_key():
 
 def test_nasa_identifier():
     http_c = http.HTTPConnection()
-    response = http_c.send_request(param="natural", sub_param="date", value="2019-05-30")
+    response = http_c.send_request(method="GET", param="natural", sub_param="date", value="2019-05-30")
 
     assert response.status_code == 200, f"Request failed with status code {response.status_code}"
 
     data = response.json()
     log.logger.info(f"Response Data: {data}")
     assert 'identifier' in data[0], "identifier not found in API response"
-
-
-def test_successful_data_retrieval():
-    response = requests.get("https://epic.gsfc.nasa.gov/api/natural")
-    assert response.status_code == 200  # Should be 200, expect failure here
