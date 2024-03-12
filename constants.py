e2ddc0277cc4caad9dded177d48de119b360a815
sandro-ua <qatestermailbox@gmail.com>
2024-03-12 22:00:24 +0200

    Add get env var method

--- constants.py
+++ constants.py
@@ -1,4 +1,14 @@
 import os
+import logger as log
+
+
+def get_env_var(env_var):
+    try:
+        return os.environ.get(env_var)
+    except KeyError:
+        log.logger.error(f"{env_var} environment variable not found.")
+        return None
+
 
 BASE_URL = "https://api.nasa.gov"
-API_KEY = os.environ.get('API_KEY')
+API_KEY = get_env_var('API_KEY')
